34f1b26b9af334618fd44799d600429a0c811887
mattyw <gh@mattyw.net>
2015-02-05 19:11:36 +0800

    we can skip numbers that we have already seen to make things faster

--- collatz.go
+++ collatz.go
@@ -24,8 +24,26 @@ func collatzSeq(start int) []int {
 
 }
 
+func collatzSeqOneness(oneness map[int]bool, n int) bool {
+	_, ok := oneness[n]
+	return ok
+}
+
 func main() {
+	oneness := map[int]bool{}
 	for i := 1; i < 100; i++ {
-		fmt.Println(collatzSeq(i))
+		fmt.Printf("trying %v\n", i)
+		if collatzSeqOneness(oneness, i) {
+			fmt.Printf("skipping %v, we know it has oneness\n", i)
+			continue
+		}
+		ls := collatzSeq(i)
+		for _, i := range ls {
+			oneness[i] = true // For every item in the seq we ended up at 1, therefore we know all the items in this seq have oneness.
+		}
 	}
+	for k, _ := range oneness {
+		fmt.Printf("%v, ", k)
+	}
+	fmt.Println("")
 }
